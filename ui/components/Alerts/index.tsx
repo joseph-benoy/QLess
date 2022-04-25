2c3c48241fb57418cf303f63718edd6d95065e00
joseph-benoy <josephbenoy03@gmail.com>
2022-04-25 22:57:25 +0530

    created alert HOC

---
+++ ui/components/Alerts/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import {CgClose} from "react-icons/cg";
+
+
+export interface IInfoProps {
+    text:string,
+    children?:React.ReactNode,
+    color?:string,
+    show:boolean
+}
+
+export default function Alert (props: IInfoProps) {
+    const close = ()=>{
+        document.getElementById("alertbox")!.style.display = "none";
+      }
+  return (
+    <>
+        <div>
+            {
+                props.children
+            }
+            <div className='alert' id="alertbox">
+                <div className={`flex bg-${props.color?props.color:"blue"}-500 px-5 pl-6 py-3 w-fit items-center rounded gap-2 shadow-2xl`}>
+                    <p className='text-white font-medium'>{props.text}</p>
+                    <CgClose className='text-white font-bold' onClick={close}/>
+                </div>  
+            </div>
+        </div>
+    </>
+  );
+}
--- ui/pages/user/index.tsx
+++ ui/pages/user/index.tsx
@@ -1,13 +1,19 @@
 import * as React from 'react';
+import Alert from '../../components/Alerts';
 import Dashboard from '../../layouts/dashboard';
 
 export interface IOverViewProps {
 }
 
 export default function OverView (props: IOverViewProps) {
+
   return (
+    <>
+    <Alert text='test messaege' color='green' show={true}>
     <Dashboard>
-      <h1>Overview</h1>
+      <h1>OverView</h1>
     </Dashboard>
+    </Alert>
+    </>
   );
 }
