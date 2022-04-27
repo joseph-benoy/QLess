944cf0e89703e00c0afdd83cddb9c3e6b972d10a
joseph-benoy <josephbenoy03@gmail.com>
2022-04-27 12:30:35 +0530

    added modules base

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { EventService } from './event/event.service';
+import { EventController } from './event/event.controller';
+import { EventModule } from './event/event.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, AuthModule, EventModule],
+  controllers: [AppController, UserController, EventController],
+  providers: [EventService],
 })
 export class AppModule {}
