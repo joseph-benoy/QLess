2b2fbffe962a1c89799e2e20608f6c01752a0998
joseph-benoy <josephbenoy03@gmail.com>
2022-04-28 00:05:38 +0530

    addded jwt-strategy , getUser decorator

---
+++ server/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,26 @@
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService, private readonly prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+  async validate(payload: { sub: number; email: string }) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          id: payload.sub,
+        },
+      });
+      delete user.password;
+      return user;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
